00d63d8bb71d6d45354b07066e391340b5a0b03f
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2025-03-17 21:05:49 -0400

    refactoring to return PortObjectOptimized reather than a String

--- src/rules/rule/port_object/mod.rs
+++ src/rules/rule/port_object/mod.rs
@@ -20,6 +20,16 @@ pub enum PortObjectItem {
     Group(Group),
 }
 
+/// Vector of PortObjectItem returned after optimization  
+/// name - all opertations performed on items  
+/// items - the list of PortList objects  
+/// PortList objects are flattened from the Group objects and normal PortList objects
+#[derive(Debug)]
+pub struct PortObjectOptimized {
+    name: String,
+    items: Vec<PortList>,
+}
+
 #[derive(thiserror::Error, Debug)]
 pub enum PortObjectError {
     #[error("Failed to parse port object: {0}")]
@@ -120,7 +130,24 @@ fn unique_l3_items(port_lists: Vec<&PortList>) -> Vec<&PortList> {
     unique_items
 }
 
-fn optimize_l4_items(port_lists: Vec<&PortList>) -> Vec<PortList> {
+impl PortObjectOptimized {
+    pub fn new(port_list: &PortList) -> Self {
+        PortObjectOptimized {
+            name: port_list.get_name().to_string(),
+            items: vec![port_list.clone()],
+        }
+    }
+
+    pub fn append(&mut self, port_list: &PortList) {
+        self.items.push(port_list.clone());
+    }
+
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+}
+
+fn optimize_l4_items(port_lists: Vec<&PortList>) -> Vec<PortObjectOptimized> {
     let mut port_lists = port_lists;
     port_lists.sort_by_key(|item| ((item.get_protocol() as u32) << 16) + item.get_ports().0 as u32);
 
@@ -130,6 +157,7 @@ fn optimize_l4_items(port_lists: Vec<&PortList>) -> Vec<PortList> {
         return optimized_port_lists;
     }
 
+    let mut optimized_port_list = PortObjectOptimized::new(&port_lists[0]);
     let mut current_port_list = port_lists[0].clone();
 
     for next_port_list in port_lists.into_iter().skip(1) {
@@ -146,23 +174,30 @@ fn optimize_l4_items(port_lists: Vec<&PortList>) -> Vec<PortList> {
                 );
 
                 let tcp_udp_obj = tcp_udp::Builder::new()
-                    .name(new_name)
+                    .name(new_name.clone())
                     .protocol(current_port_list.get_protocol())
                     .start(curr_start)
                     .end(max(curr_end, next_end))
                     .build();
                 current_port_list = port_list::PortList::TcpUdp(tcp_udp_obj);
+
+                optimized_port_list.append(&next_port_list);
+                optimized_port_list.set_name(new_name);
             } else {
-                optimized_port_lists.push(current_port_list);
                 current_port_list = next_port_list.clone();
+
+                optimized_port_lists.push(optimized_port_list);
+                optimized_port_list = PortObjectOptimized::new(next_port_list);
             }
         } else {
-            optimized_port_lists.push(current_port_list);
             current_port_list = next_port_list.clone();
+
+            optimized_port_lists.push(optimized_port_list);
+            optimized_port_list = PortObjectOptimized::new(next_port_list);
         }
     }
 
-    optimized_port_lists.push(current_port_list);
+    optimized_port_lists.push(optimized_port_list);
 
     optimized_port_lists
 }
@@ -1024,6 +1059,6 @@ mod tests {
 
         let optimized = optimize_l4_items(port_lists);
         dbg!(&optimized);
-        assert_eq!(optimized.len(), 7);
+        assert_eq!(optimized.len(), 5);
     }
 }
